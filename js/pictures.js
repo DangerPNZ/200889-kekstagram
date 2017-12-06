7cdd410ff9dcdb12f1f4993be9829637429f2cbc
DangerPNZ <dangerpnz@mail.ru>
2017-12-06 22:04:18 +0300

    Таск2

--- js/pictures.js
+++ js/pictures.js
@@ -77,7 +77,7 @@ var fillBlockPictures = function (info) {
 
 var postsInfo = generatePostInfo();
 var galleryOverlay = document.querySelector('.gallery-overlay');
-var closeBtn = galleryOverlay.querySelector('.gallery-overlay-close');
+var closeGalleryBtn = galleryOverlay.querySelector('.gallery-overlay-close');
 
 var fillGalleryOverlay = function (object) {
   galleryOverlay.classList.remove('hidden');
@@ -87,8 +87,8 @@ var fillGalleryOverlay = function (object) {
 };
 
 var removeCloseHandlers = function () {
-  closeBtn.removeEventListener('click', clickCloseBtnHandler);
-  closeBtn.removeEventListener('keydown', pressCloseBtnHandler);
+  closeGalleryBtn.removeEventListener('click', clickCloseBtnHandler);
+  closeGalleryBtn.removeEventListener('keydown', pressCloseBtnHandler);
   document.removeEventListener('keydown', escBtnHandler);
 };
 
@@ -106,15 +106,15 @@ var pressCloseBtnHandler = function (event) {
 };
 
 var escBtnHandler = function (event) {
-  if (event.keyCode === ESC_KEY_CODE && !galleryOverlay.classList.contains('hidden')) {
+  if (event.keyCode === ESC_KEY_CODE) {
     galleryOverlay.classList.add('hidden');
     removeCloseHandlers();
   }
 };
 
 var addCloseHandlers = function () {
-  closeBtn.addEventListener('click', clickCloseBtnHandler);
-  closeBtn.addEventListener('keydown', pressCloseBtnHandler);
+  closeGalleryBtn.addEventListener('click', clickCloseBtnHandler);
+  closeGalleryBtn.addEventListener('keydown', pressCloseBtnHandler);
   document.addEventListener('keydown', escBtnHandler);
 };
 
@@ -132,5 +132,92 @@ var addHandlers = function () {
   }
 };
 
+/* module4-task2 */
+var uploadImageForm = document.querySelector('#upload-select-image');
+var uploadImageInput = uploadImageForm.querySelector('#upload-file');
+var uploadOverlay = uploadImageForm.querySelector('.upload-overlay');
+var uploadCloseBtn = uploadImageForm.querySelector('.upload-form-cancel');
+var effectSwitches = uploadImageForm.querySelectorAll('input[name="effect"]');
+var image = uploadImageForm.querySelector('.effect-image-preview');
+
+var uploadChangeHandler = function () {
+  uploadOverlay.classList.remove('hidden');
+  addUploadCloseHandlers();
+};
+
+uploadImageInput.addEventListener('change', uploadChangeHandler);
+
+var removeUploadCloseHandlers = function () {
+  uploadCloseBtn.removeEventListener('click', clickUploadCloseBtnHandler);
+  uploadCloseBtn.removeEventListener('keydown', pressUploadCloseBtnHandler);
+  document.removeEventListener('keydown', escUploadBtnHandler);
+};
+
+var clickUploadCloseBtnHandler = function (event) {
+  event.preventDefault();
+  uploadOverlay.classList.add('hidden');
+  removeUploadCloseHandlers();
+};
+
+var pressUploadCloseBtnHandler = function (event) {
+  if (event.keyCode === ENTER_KEY_CODE) {
+    uploadOverlay.classList.add('hidden');
+    removeUploadCloseHandlers();
+  }
+};
+
+var escUploadBtnHandler = function (event) {
+  if (event.keyCode === ESC_KEY_CODE) {
+    uploadOverlay.classList.add('hidden');
+    removeUploadCloseHandlers();
+  }
+};
+
+var addUploadCloseHandlers = function () {
+  uploadCloseBtn.addEventListener('click', clickUploadCloseBtnHandler);
+  uploadCloseBtn.addEventListener('keydown', pressUploadCloseBtnHandler);
+  document.addEventListener('keydown', escUploadBtnHandler);
+};
+
+var changeScaleValue = function () {
+  var scaleValueInput = uploadImageForm.querySelector('.upload-resize-controls-value');
+  var btnMinus = uploadImageForm.querySelector('.upload-resize-controls-button-dec');
+  var btnPlus = uploadImageForm.querySelector('.upload-resize-controls-button-inc');
+  var scaleChangeStep = 25;
+  var initialValue = scaleValueInput.value;
+  var currentValue = scaleValueInput.value;
+
+  btnMinus.addEventListener('click', function () {
+    if (currentValue > 0) {
+      scaleValueInput.value = String(parseInt(currentValue, 10) - scaleChangeStep + '%');
+    }
+  });
+};
+
+var cleanEffects = function () {
+  image.classList.remove('effect-none');
+  image.classList.remove('effect-chrome');
+  image.classList.remove('effect-sepia');
+  image.classList.remove('effect-marvin');
+  image.classList.remove('effect-phobos');
+  image.classList.remove('effect-heat');
+};
+var selectEffect = function (event) {
+  cleanEffects();
+  // var effect = event.currentTarget.id
+  // effect = effect.slice(7)
+  image.classList.add('effect-' + event.currentTarget.value);
+};
+
+var addEffectSelectHandlers = function () {
+  for (var a = 0; a < effectSwitches.length; a++) {
+    effectSwitches[a].addEventListener('change', selectEffect);
+  }
+};
+
 fillBlockPictures(postsInfo);
 addHandlers();
+
+//
+changeScaleValue();
+addEffectSelectHandlers();
