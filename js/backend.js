c6c35930f6ffcdf21e9435bdd1c9464e15b437c1
DangerPNZ <dangerpnz@mail.ru>
2017-12-23 22:51:00 +0300

    бакенд правка

--- js/backend.js
+++ js/backend.js
@@ -1,1 +1,48 @@
-// перламутровые пуговицы
+'use strict';
+
+(function () {
+  window.backend = {
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        switch (xhr.status) {
+          case 200:
+            onLoad(JSON.parse(xhr.responseText));
+            break;
+          case 301:
+            onError('Страница перемещена на другой адрес');
+            break;
+          case 302:
+            onError('Страница временно перемещена на другой адрес');
+            break;
+          case 401:
+            onError('Ошибка запроса');
+            break;
+          case 403:
+            onError('Доступ запрещен');
+            break;
+          case 404:
+            onError('Ничего не найдено');
+            break;
+          case 500:
+            onError('Ошибка сервера');
+            break;
+          default:
+            onError(xhr.status + ' ' + xhr.statusText);
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+      xhr.timeout = 5000;
+      xhr.open('GET', window.constants.DOWNLOAD_URL);
+      xhr.send();
+    }
+  };
+})();
--- js/constants.js
+++ js/constants.js
@@ -9,6 +9,8 @@
     SCALE_CHANGE_STEP: 25,
     SCALE_MIN_VALUE: 25,
     SCALE_MAX_VALUE: 100,
-    SCALE_START_VALUE: 50
+    SCALE_START_VALUE: 50,
+    DOWNLOAD_URL: '//js.dump.academy/kekstagram/data',
+    UPLOAD_URL: 'https://js.dump.academy/kekstagram'
   };
 })();
